8e63e2db2be90e6340cb32096ba77303cca3fa73
jadilovic <adilovic79@yahoo.com>
2022-11-30 10:58:33 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <AddressFieldGroupVue msg="Vue Form" label="Address" v-model="streetName" />
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import AddressFieldGroupVue from './components/AddressFieldGroup.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    AddressFieldGroupVue
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/AddressFieldGroup.vue
@@ -0,0 +1,84 @@
+<template>
+  <section class="address">
+    <h1>{{msg}}</h1>
+    <h2>{{ label }}</h2>
+    <div>
+        <input type="text" v-model="value">
+        <p>Value: {{value}}</p>
+    </div>
+    <div class="address__field">
+      <label for="streetName">Street name</label>
+      <input
+        type="text"
+        id="streetName"
+        :value="streetName"
+        @input="$emit('update:streetName', $event.target.value)"
+        required
+      />
+      <label for="street">Display:</label>
+      <p id="street">{{streetName}}</p>
+    </div>
+    <div class="address__field">
+      <label for="streetNumber">Street number</label>
+      <input
+        type="text"
+        id="streetNumber"
+        :value="streetNumber"
+        @input="$emit('update:streetNumber', $event.target.value)"
+        required
+      />
+    </div>
+    <div class="address__field">
+      <label for="city">City</label>
+      <input
+        type="text"
+        id="city"
+        :value="city"
+        @input="$emit('update:city', $event.target.value)"
+        required
+      />
+    </div>
+    <div class="address__field">
+      <label for="postcode">Postcode</label>
+      <input
+        type="text"
+        id="postcode"
+        :value="postcode"
+        @input="$emit('update:postcode', $event.target.value)"
+        required
+      />
+    </div>
+  </section>
+</template>
+
+<script setup>
+import {ref} from "vue";
+const value = ref("");
+
+// export default {
+//   name: "AddressFieldGroup",
+//   props: {
+//     msg: String,
+//     label: {
+//       type: String,
+//       default: "",
+//     },
+//     streetName: {
+//       type: String,
+//       default: "",
+//     },
+//     streetNumber: {
+//       type: String,
+//       default: "",
+//     },
+//     city: {
+//       type: String,
+//       default: "",
+//     },
+//     postcode: {
+//       type: String,
+//       default: "",
+//     },
+//   },
+// };
+</script>
